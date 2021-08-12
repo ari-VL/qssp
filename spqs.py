473d919848f307a76d1e4e14f7a05fc411796e6d
Ariadna Venegas-Li <arivli14@gmail.com>
2021-08-12 15:18:23 -0700

    updated files

--- spqs.py
+++ spqs.py
@@ -30,3 +30,76 @@ Methods:
     msp_construction
     others
 '''
+
+import numpy as np
+import scipy.linalg as la
+from scipy.stats import entropy
+
+import hmm
+import q_utils
+
+class qsHMM:
+
+    def __init__(self, HMM, alph):
+        #should it take in Trans Matrices?
+        self.HMM=HMM
+        self.alph= alph
+        self.alph_size= len(alph)
+        #check alph size is equal to no. of Ts
+
+    def q_word(self, c_word,L):
+        #takes in a classical word and turns it into a quntum state word
+        q_word = self.alph[int(c_word[0])].state
+        for i in range(1,len(c_word)):
+            q_word = np.kron(q_word, self.alph[int(c_word[i])].state)
+        q_word = qstate(q_word)
+        return q_word
+        
+    def q_words(self, n_words, L):
+        #samples n_words of length L 
+        c_words = self.HMM.sample_words(n_words,L)
+        q_words = [self.q_word(c_word, L) for c_word in c_words]
+        return q_words
+        
+    def q_block(self, L, join=True):
+        #calculates of the words of length L and their probs and returns the joint quantum state
+        #TODO: test 
+        c_words, c_probs = self.HMM.all_words(L)
+        if join:
+            q_block = c_probs[0] * self.q_word(c_words[0],L).state
+            for i in range(1,len(c_words)):
+                q_block = q_block + c_probs[i]*self.q_word(c_words[i],L).state
+            q_block = qstate(q_block)
+                return q_block
+        else: 
+            q_seq = []
+            for i in range(len(c_words)):
+                q_seq.append(self.q_word(c_words[i],L).state)
+            return q_seq, c_probs
+
+    def q_block_entropies(self, L):
+        if L > 0:
+            ents = [self.q_block(l).vn_entropy() for l in range(1,L+1)]
+        else:
+            ents = []
+            print("ERROR: Invalid block length (must be a positive integer)")
+        return ents
+        
+    def q_entropy_rate(self, L):
+        if L > 1:
+            s_est = self.q_block(L).vn_entropy() - self.q_block(L-1).vn_entropy()
+        else:
+            s_est = 0
+            print("ERROR: Invalid block length (must be greater than 1)")
+        return s_est
+        
+    def q_excess_entropy(self, L):
+        if L > 1:
+            EE_est = self.q_block(L).vn_entropy() - L * self.q_entropy_rate(L)
+        else:
+            EE_est = 0
+            print("ERROR: Invalid block length (must be greater than 1)")
+        return EE_est
+
+    def get_measured_machine(self, measurement):
+        pass
