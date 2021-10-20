3833eb49613df4fa171705a13f7312bbc448a040
dgier <david.gier7@gmail.com>
2021-10-19 22:31:29 -0700

    Added observer state uncertainty calculation

--- qshmm.py
+++ qshmm.py
@@ -5,6 +5,9 @@ from scipy.stats import entropy
 from .hmm import HMM
 from .q_objects import qstate
 
+def near(a, b, rtol = 1e-5, atol = 1e-8):
+    return np.abs(a-b)<(atol+rtol*np.abs(b))
+
 class qsHMM:
     '''
     A quantum state Hidden Markov Model consisting of a classical HMM
@@ -28,7 +31,7 @@ class qsHMM:
     q_block(L, join = True)
         Returns single quantum state represetning all words of length L
         or (if join == False) a quantum state and probability for each word
-    q_block_entropies(L):
+    q_block_entropies(L)
         Returns a list of von Neumann entropies for blocks of length=1 to length=L
     q_entropy_rate(L)
         Computes and returns the (difference) von Neumann entropy rate approximation at length L
@@ -36,6 +39,8 @@ class qsHMM:
         Returns the quantum excess entropy approximation at length L
     get_measured_machine(measurement)
         Returns the classical HMM generator corresponding to a particular measurement basis
+    observer_state_uncertainty(measurement, L, ever_synched = False)
+        Returns average state uncertainty of observer measuring in fixed basis on up to L sites
     '''
 
     def __init__(self, HMM, alph):
@@ -124,3 +129,68 @@ class qsHMM:
         #create HMM object with the correct transition matrices
         measured_mach = HMM(measured_Ts)
         return measured_mach
+
+    def observer_state_uncertainty(self,measurement,L,ever_synched=False):
+        '''
+        Returns average state uncertainty of observer measuring in fixed basis on up to L sites
+        '''
+
+        S = [[self.HMM.init]]
+        probs = [[1]]
+        H_avg = [entropy(S[0][0],base=2)]
+        
+        meas = self.get_measured_machine(measurement)
+        Ts = meas.Ts
+        
+        for l in range(1,L+1):
+            S_t = []
+            probs_t = []
+            H_avg_t = 0
+
+            for s in range(len(S[-1])):
+                # Calculates mixed states given L observations using mixed states given (L-1) observations
+                S_prev = S[-1][s]
+                prob_prev = probs[-1][s]
+
+                if(ever_synched and self.is_synched(S_prev)):
+                    # If 'ever_synched', stop calculating new mixed states after synchronizing observation
+                    S_t.append(S_prev)
+                    probs_t.append(prob_prev)
+                
+                else:
+                    for i in range(len(meas.Ts)):
+                        S_new = [0] * len(meas.Ts[0])
+
+                        for k in range(len(meas.Ts[0])):
+                            prob_k = 0
+
+                            for j in range(len(meas.Ts[0])):
+                                if not near(Ts[i][j][k],0):
+                                    # Calculate probability of each new mixed state
+                                    prob_k += S_prev[j]* Ts[i][j][k]
+
+                            S_new[k] += prob_k
+
+                        if not near(sum(S_new),0):
+                            S_t.append(S_new/sum(S_new))
+                            new_prob = prob_prev*sum(S_new)
+                            probs_t.append(new_prob)
+                            H_avg_t += new_prob * entropy(S_new/sum(S_new))
+
+            S.append(S_t)
+            probs.append(probs_t)
+            H_avg.append(H_avg_t)
+
+        # Returns list of entropies over mixed states for different L values
+        return H_avg
+
+    def is_synched(mixed_state):
+        # If mixed state is concentrated in one generator state returns True, else False
+        if not near(sum(mixed_state),1):
+            print("ERROR: State not normalized.")
+        
+        for s in mixed_state:
+            if (not near(s,0) and not near(s,1)):
+                return False
+        
+        return True
